fe792f26daccc485c6a27d3316be17b596f74629
VishnuVardhan45 <thallapa@ds.ad.adp.com>
2016-12-29 11:11:48 +0530

    merge

--- Scripts/Application/App.js
+++ Scripts/Application/App.js
@@ -2,8 +2,8 @@ var app = angular.module('App', ['ngRoute']);
 
 
 
+
 app.config(['$routeProvider', function ($routeProvider) {
-<<<<<<< HEAD
   $routeProvider.
   when('/home', {
     templateUrl: 'Views/Home.html',
@@ -35,37 +35,5 @@ app.config(['$routeProvider', function ($routeProvider) {
   }).otherwise({
     redirectTo: 'home'
   });
-=======
-    $routeProvider.
-        when('/home', {
-            templateUrl: 'Views/Home.html',
-            controller: 'defaultController',
-            activetab: 'home'
-        }).when('/about', {
-            templateUrl: 'Views/About.html',
-			controller: 'defaultController',
-            activetab: 'about'
-        }).when('/contact', {
-            templateUrl: 'Views/Contact.html',
-			controller: 'defaultController',
-            activetab: 'contact'
-        }).when('/bookOnline', {
-            templateUrl: 'Views/BookOnline.html',
-			controller: 'defaultController',
-			activetab: 'bookOnline',			
-        }).when('/events',{
-			templateUrl: 'Views/Events.html',
-			controller : 'eventsPageCtrl',
-			activetab: 'events',
-		}).when('/eventDetailsPage',{
-			templateUrl: 'Views/eventDetailsPage.html',
-			controller : 'eventDetailsPageCtrl',
-			activetab: 'events',
-		}).when('/custRegister',{
-			templateUrl: 'Views/registerPage.html',
-			controller: 'addCustsCtrl'
-		}).otherwise({
-            redirectTo: 'home'
-        });
->>>>>>> 78bb826f7c8f752a665d56aec3f6d6352943858f
+
 }]);
