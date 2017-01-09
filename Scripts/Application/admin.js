a3792864c7df9de2b8ef6989b44b72f6b34c6983
VishnuVardhan45 <Vishnuvardhan.Yeddula@cdk.com>
2017-01-09 09:31:26 +0530

    modified code for pagination

--- Scripts/Application/admin.js
+++ Scripts/Application/admin.js
@@ -54,6 +54,7 @@
 			});
 			app.filter('startFrom', function() {
 			    return function(input, start) {
+						if (!input || !input.length) { return; }
 			        start = +start; //parse to int
 			        return input.slice(start);
 			    }
